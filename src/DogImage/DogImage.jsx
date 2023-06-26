1b4e28a31f0f0cf77d2c19d2e66e43ceb8389535
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2023-06-26 20:50:28 +0300

    more

--- src/DogImage/DogImage.jsx
+++ src/DogImage/DogImage.jsx
@@ -1,12 +1,17 @@
+import { useState } from "react"
+
 const DogImage = ({size, url}) => {
 
+    const [imgLoaded, setImgLoaded] = useState(false)
 
     const handleFinishedLoading = () => {
         console.log('finished loading')
+        setImgLoaded(true)
     }
 
     return(
-        <img style={{width: size, height: size, objectFit: 'cover', borderRadius: '15px'}}
+        <img style={{width: size, height: size, objectFit: 'cover', borderRadius: '15px', 
+            visibility: imgLoaded ? 'visible' : 'hidden'}}
             src={url} onLoad={handleFinishedLoading}
         />
     )
